1aedb9943b44decf093e3c005742942fd1f837ea
Sean Capelle <seancapelle@gmail.com>
2016-11-22 15:50:11 -0500

    Added Angular

--- public/config/app.js
+++ public/config/app.js
@@ -1,46 +1,45 @@
 (function(){
-	var app = angular.module('trade', [ ]);
+	var app = angular.module('cardTrade', []);
 
 	app.controller('TradeController', function(){
-		this.have = haveCard;
-		this.want = wantCard;
-
-		this.trade.have.id = "Abiam";
-
-		//Card you have to trade
-	var haveCard = [
-		{
-			products: 'products',
-			product: 'product',
-			id: 'id',
-			hiprice: 'hiprice',
-			lowprice: 'lowprice',
-			avgprice: 'avgprice',
-			link: 'link'
-		}
-
-	];
-	//Card you want from trade
-	var wantCard = [
-		{
-			products: 'products',
-			product: 'product',
-			id: 'id',
-			hiprice: 'hiprice',
-			lowprice: 'lowprice',
-			avgprice: 'avgprice',
-			link: 'link'
-		}
-	];
-
-	var model = [
-		card1, card2, card3, card4
-	]
-	function clicked(cardName) {
-		
-	}
-
-
-
+		this.your = yourCard;
+		this.want = wantCard;	
 	});
+		
+	//Card you have to trade
+	var yourCard =
+		{
+			products: 'products',
+			product: 'product',
+			id: 'id',
+			hiprice: 'hiprice',
+			lowprice: 'lowprice',
+			avgprice: 'avgprice',
+			link: 'link'
+		};
+
+	//Card you want from trade
+	var wantCard = 
+		{
+			products: 'products',
+			product: 'product',
+			id: 'id',
+			hiprice: 'hiprice',
+			lowprice: 'lowprice',
+			avgprice: 'avgprice',
+			link: 'link'
+		};
+
+
+
+	// var model = [
+	// 	card1, card2, card3, card4
+	// ]
+	// function clicked(cardName) {
+		
+	// }
+
+
+
+	
 })();
